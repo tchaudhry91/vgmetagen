66fcb6f93d5241a29a5ffa8a1ce3c62810e88574
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2017-09-26 17:58:58 +0530

    Added a Game model

--- models.go
+++ models.go
@@ -12,6 +12,17 @@ type GamesResponse struct {
 	Version              string         `json:"version"`
 }
 
+type GameGiantBombResponse struct {
+	ErrorStatus          string `json:"error"`
+	Limit                int    `json:"limit"`
+	Offset               int    `json:"offset"`
+	NumberOfPageResults  int    `json:"number_of_page_results"`
+	NumberOfTotalResults int    `json:"number_of_total_results"`
+	StatusCode           int    `json:"status_code"`
+	Results              Game   `json:"results"`
+	Version              string `json:"version"`
+}
+
 // GameResponse holds information for a game's Name and it's GiantBomb ID
 type GameResponse struct {
 	Name string `json:"name"`
@@ -21,6 +32,39 @@ type GameResponse struct {
 // GamesDirectory holds information for Games in a map
 type GamesDirectory struct {
 	Games map[int]string `json:"games"`
+}
+
+// Game holds all the meta information for a Game
+type Game struct {
+	Aliases             string `json:"aliases"`
+	ID                  int    `json:"id"`
+	Name                string `json:"name"`
+	OriginalReleaseDate string `json:"original_release_date"`
+	Platforms           []struct {
+		APIDetailURL  string `json:"api_detail_url"`
+		ID            int    `json:"id"`
+		Name          string `json:"name"`
+		SiteDetailURL string `json:"site_detail_url"`
+		Abbreviation  string `json:"abbreviation"`
+	} `json:"platforms"`
+	Developers []struct {
+		APIDetailURL  string `json:"api_detail_url"`
+		ID            int    `json:"id"`
+		Name          string `json:"name"`
+		SiteDetailURL string `json:"site_detail_url"`
+	} `json:"developers"`
+	Publishers []struct {
+		APIDetailURL  string `json:"api_detail_url"`
+		ID            int    `json:"id"`
+		Name          string `json:"name"`
+		SiteDetailURL string `json:"site_detail_url"`
+	} `json:"publishers"`
+	Concepts []struct {
+		APIDetailURL  string `json:"api_detail_url"`
+		ID            int    `json:"id"`
+		Name          string `json:"name"`
+		SiteDetailURL string `json:"site_detail_url"`
+	}
 }
 
 // initMap initializes the map in the directory
